7341f11b7bfc40d8a6ccc9afa2e36ee41c202747
homerjay <homerjay@naver.com>
2021-03-27 17:27:22 +0900

    Circleprogressindicator error NoData

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carrot/repository/contents_repository.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 
@@ -8,13 +9,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, String>> datas = [];
+  ContentsRepository contentsRepository;
   String _curruntLocation;
-  final Map<String,String> locationTypeToString = {           //밑에서 value를 키로 받은다음
-    "ara" : "아라동",                                         //키에다가 넣어준다 ㅠㅠ
-    "ildong":"일동",
-    "idong":"이동",
-    "sadong":"사동",
+  final Map<String, String> locationTypeToString = { //밑에서 value를 키로 받은다음
+    "ara": "아라동", //키에다가 넣어준다 ㅠㅠ
+    "ora": "오라동",
+    "idong": "이동",
+    "sadong": "사동",
   };
 
   @override
@@ -22,82 +23,19 @@ class _HomeState extends State<Home> {
     // TODO: implement initState
     super.initState();
     _curruntLocation = "ara";
-
-    datas = [
-      {
-        "image": "assets/images/1.jpg",
-        "title": "네메시스 축구화275",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/2.jpg",
-        "title": "LA갈비 5kg팔아요~",
-        "location": "제주 제주시 아라동",
-        "price": "100000",
-        "likes": "5"
-      },
-      {
-        "image": "assets/images/3.jpg",
-        "title": "치약팝니다",
-        "location": "제주 제주시 아라동",
-        "price": "5000",
-        "likes": "0"
-      },
-      {
-        "image": "assets/images/4.jpg",
-        "title": "[풀박스]맥북프로16인치 터치바 실버그레이 ",
-        "location": "제주 제주시 아라동",
-        "price": "2500000",
-        "likes": "6"
-      },
-      {
-        "image": "assets/images/5.jpg",
-        "title": "디월트존기임팩",
-        "location": "제주 제주시 아라동",
-        "price": "150000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/6.jpg",
-        "title": "갤럭시s10",
-        "location": "제주 제주시 아라동",
-        "price": "180000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/7.jpg",
-        "title": "선반",
-        "location": "제주 제주시 아라동",
-        "price": "15000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/8.jpg",
-        "title": "냉장 쇼케이스",
-        "location": "제주 제주시 아라동",
-        "price": "80000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/9.jpg",
-        "title": "대우 미니냉장고",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/10.jpg",
-        "title": "멜킨스 풀업 턱걸이 판매합니다.",
-        "location": "제주 제주시 아라동",
-        "price": "50000",
-        "likes": "7"
-      },
-    ];
   }
 
-  Widget _bodyWidget() {
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    contentsRepository = ContentsRepository();
+  }
+
+  _loadContent(){
+    return contentsRepository.loadContentsFormLoaction(_curruntLocation);  //로드컨텐트를 리턴해줌 //데이터들
+  }
+
+  _makeDataList(List<Map<String,String>> datas){
     return ListView.separated(
       //세퍼레이트는 개별개별의 아이템마다 사이간격 라인을 손쉽게 나눌수 잇게해줌
       padding: EdgeInsets.symmetric(horizontal: 10),
@@ -107,7 +45,7 @@ class _HomeState extends State<Home> {
           child: Row(
             children: [
               ClipRRect(
-                  //ClipRRect 로 보더레디우스 올 전체값을 레디우스 서클로 20줌
+                //ClipRRect 로 보더레디우스 올 전체값을 레디우스 서클로 20줌
                   borderRadius: BorderRadius.all(Radius.circular(10)),
                   child: Image.asset(
                     datas[index]["image"],
@@ -133,7 +71,8 @@ class _HomeState extends State<Home> {
                       Text(
                         datas[index]["location"],
                         style: TextStyle(
-                            fontSize: 12, color: Colors.black.withOpacity(0.3)),
+                            fontSize: 12,
+                            color: Colors.black.withOpacity(0.3)),
                       ),
                       SizedBox(
                         height: 5,
@@ -178,83 +117,109 @@ class _HomeState extends State<Home> {
           color: Colors.black.withOpacity(0.4),
         );
       },
-      itemCount: datas.length,
+      itemCount: 10,
+    );
+  }
+
+  Widget _bodyWidget() {
+    return FutureBuilder(
+      future: _loadContent(), //loadcontent를 불러오고 (어떤 데이터가 오기 모르기때문에 퓨처로..)
+      builder: (content,snapshot) { //오브젝트를 만들거나 클래스를 선언할떄 dynamic에 지정가능
+        if(snapshot.connectionState != ConnectionState.done){       //커넥션 스테이트가 끝났는지 안긑났는지 !
+          return Center(child: CircularProgressIndicator());         //데이터를 받고있는경우 로딩처리해줌.
+        }
+        if(snapshot.hasError){
+          return Center(child: Text('데이터 오류가 났습니다.'),);
+        }
+
+        if(snapshot.hasData){
+         return _makeDataList(snapshot.data);
+        }
+
+        return Center(child: Text('해당지역에 데이터가 없습니다.'),);  //지역에 데이터가없으면
+      },
     );
   }
 
 
-  Widget appBarWidget(){
-    return AppBar(
-      title: GestureDetector(
-        onTap: (){
-          print('Click');
-        },
-        // onLongPress: (){
-        //   print('Long press');
-        // },
-        child: PopupMenuButton<String>(       //pop업 메뉴버튼을 생산할 수 있음!!
-          offset: Offset(0,0),
-          shape: ShapeBorder.lerp(            //쉐이프로 팝업된 메뉴를 둥글게 만들수있음 라운드렉팅!!
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-              1
-          ),
-          onSelected: (String where) {
-            setState(() {
-              _curruntLocation = where; //밑에서 받은 value를 where에 넣고 커런트 로케이션에 넣어줌.
-            });
+    Widget appBarWidget() {
+      return AppBar(
+        title: GestureDetector(
+          onTap: () {
+            print('Click');
           },
-          itemBuilder: (context) {
-            return [
-              PopupMenuItem(value: "ara",child: Text('아라동'),),    //팝업메뉴 아이템을 설정하면 클릭시 팝업메뉴가 밑으로 쫘르륵나옴
-              PopupMenuItem(value: "ildong",child: Text('일동'),),
-              PopupMenuItem(value: "idong",child: Text('이동'),),
-              PopupMenuItem(value: "sadong",child: Text('사동'),),
-            ];
-          },
+          // onLongPress: (){
+          //   print('Long press');
+          // },
+          child: PopupMenuButton<String>( //pop업 메뉴버튼을 생산할 수 있음!!
+            offset: Offset(0, 0),
+            shape: ShapeBorder.lerp( //쉐이프로 팝업된 메뉴를 둥글게 만들수있음 라운드렉팅!!
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                1
+            ),
+            onSelected: (String where) {
+              setState(() {
+                _curruntLocation =
+                    where; //밑에서 받은 value를 where에 넣고 커런트 로케이션에 넣어줌.
+              });
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(value: "ara", child: Text('아라동'),),
+                //팝업메뉴 아이템을 설정하면 클릭시 팝업메뉴가 밑으로 쫘르륵나옴
+                PopupMenuItem(value: "ora", child: Text('오라동'),),
+                PopupMenuItem(value: "idong", child: Text('이동'),),
+                PopupMenuItem(value: "sadong", child: Text('사동'),),
+              ];
+            },
 
-          child: Row(
-            children: [
-              Text(locationTypeToString[_curruntLocation]), //커런트 로케이션을 맵의 키에넣어주면 value값이 나오게된다.
-              Icon(Icons.arrow_drop_down_outlined),
-            ],
+            child: Row(
+              children: [
+                Text(locationTypeToString[_curruntLocation]),
+                //커런트 로케이션을 맵의 키에넣어주면 value값이 나오게된다.
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
           ),
         ),
-      ),
-      elevation: 0.0,
-      actions: [
-        IconButton(icon: Icon(Icons.search), onPressed: (){}),
-        IconButton(icon: Icon(Icons.tune), onPressed: (){}),
-        IconButton(icon: SvgPicture.asset("assets/svg/bell.svg",width: 22,), onPressed: (){}),
-      ],
-    );
-  }
+        elevation: 0.0,
+        actions: [
+          IconButton(icon: Icon(Icons.search), onPressed: () {}),
+          IconButton(icon: Icon(Icons.tune), onPressed: () {}),
+          IconButton(icon: SvgPicture.asset("assets/svg/bell.svg", width: 22,),
+              onPressed: () {}),
+        ],
+      );
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBarWidget(),
-      body: _bodyWidget(),
-    );
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: appBarWidget(),
+        body: _bodyWidget(),
+      );
+    }
   }
-}
 
 //intl 라이브러리 사용
-final oCcy = new NumberFormat('#,###', "ko_KR");
-//스트링 값의 price를 받아서 won으로 바꾸는 메소드
-String calcStringToWon(String priceString) {
-  return "${oCcy.format(int.parse(priceString))}원";
-}
+  final oCcy = new NumberFormat('#,###', "ko_KR");
 
-BottomNavigationBarItem _bottomNavigationBarItem(
-    String iconName, String label) {
-  return BottomNavigationBarItem(
-      icon: Padding(
-        padding: const EdgeInsets.only(bottom: 5),
-        child: SvgPicture.asset(
-          "assets/svg/${iconName}_off.svg",
-          width: 22,
+//스트링 값의 price를 받아서 won으로 바꾸는 메소드
+  String calcStringToWon(String priceString) {
+    if(priceString == "무료나눔"){return priceString;}
+    return "${oCcy.format(int.parse(priceString))}원";
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarItem(String iconName,
+      String label) {
+    return BottomNavigationBarItem(
+        icon: Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: SvgPicture.asset(
+            "assets/svg/${iconName}_off.svg",
+            width: 22,
+          ),
         ),
-      ),
-      label: label);
-}
+        label: label);
+  }
