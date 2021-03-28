d7412d8b0ce34c312225337190d46fb932ea0a47
homerjay <homerjay@naver.com>
2021-03-28 10:37:40 +0900

    detail Userinfo

---
+++ lib/components/manor_temperat_widget.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ManorTemperature extends StatelessWidget {
+  double manorTemp;
+  int level;
+  final List<Color> temperColor = [
+    Colors.black,
+    Colors.blueAccent,
+    Colors.lightBlueAccent,
+    Colors.greenAccent,
+    Colors.orangeAccent,
+    Colors.orange
+  ];
+
+  ManorTemperature({this.manorTemp}) {
+    _calcTempLevel();
+  }
+
+  void _calcTempLevel() {
+    if (manorTemp <= 20) {
+      level = 0;
+    } else if (manorTemp > 20 && manorTemp <= 32) {
+      level = 1;
+    } else if (manorTemp > 32 && manorTemp <= 36.5) {
+      level = 2;
+    } else if (manorTemp > 36.5 && manorTemp <= 40) {
+      level = 3;
+    } else if (manorTemp > 40 && manorTemp <= 50) {
+      level = 4;
+    } else if (manorTemp > 50) {
+      level = 5;
+    }
+  }
+
+  Widget _makeTempLabelAndBar() {
+    return Container(
+      width: 60,
+      child: Column(
+        children: [
+          Text(
+            "${manorTemp}℃",
+            style: TextStyle(
+                color: temperColor[level],
+                fontSize: 18,
+                fontWeight: FontWeight.bold),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: 6,
+              color: Colors.black.withOpacity(0.2),
+              child: Row(
+                children: [
+                  Container(
+                    height: 6,
+                    color: temperColor[level],
+                    width: (60 / 99) * manorTemp,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _makeTempLabelAndBar(),
+              Container(
+                margin: const EdgeInsets.only(left: 7),
+                child: Image.asset("assets/images/level-${level}.jpg"),height: 30,width: 30,),
+            ],
+          ),
+          Text(
+            '매너온도',
+            style: TextStyle(
+              decoration: TextDecoration.underline, //언더라인
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carrot/components/manor_temperat_widget.dart';
 
 class DetailContentView extends StatefulWidget {
   Map<String,String> data;
@@ -12,6 +14,9 @@ class DetailContentView extends StatefulWidget {
 
 class _DetailContentViewState extends State<DetailContentView> {
   Size size;
+  List<Map<String,String>> imgList;
+  int _current;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -21,7 +26,15 @@ class _DetailContentViewState extends State<DetailContentView> {
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
+    _current = 0;
     size = MediaQuery.of(context).size;
+    imgList = [
+      {"id" : "0", "url" : widget.data["image"]},
+      {"id" : "1", "url" : widget.data["image"]},
+      {"id" : "2", "url" : widget.data["image"]},
+      {"id" : "3", "url" : widget.data["image"]},
+      {"id" : "4", "url" : widget.data["image"]},
+    ];
   }
 
   @override
@@ -31,14 +44,25 @@ class _DetailContentViewState extends State<DetailContentView> {
         extendBodyBehindAppBar: true,  //body 이하로 확장하겠다는
         appBar: appBarWidget(),
         body: _bodyWidget(),
+        bottomNavigationBar: _bottomNavigation(),
       ),
+    );
+  }
+  
+  Widget _bottomNavigation(){
+    return Container(
+      width: size.width,
+      height: 55,
+      color: Colors.red,
     );
   }
 
   Widget appBarWidget(){
     return AppBar(
       backgroundColor: Colors.transparent,
-
+      iconTheme: IconThemeData(
+        color: Colors.white
+      ),
       elevation: 0.0,
       actions: [
         IconButton(icon: Icon(Icons.share), onPressed: () {}),
@@ -47,15 +71,90 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
-Widget _bodyWidget(){
-  return Container(
-    child: Hero(        //Hero로 왔다갔다할 때 이미지가 스타일리쉬적으로 넘어갈수잇게
-      tag: widget.data["cid"],  //데이터에서 추가한 cid로 판별
-      child: Image.asset(widget.data["image"],
-        width: size.width,
-      fit: BoxFit.fill,
+  Widget _makeSilderImage(){
+    return Container(
+      child: Stack(
+        children: [
+          Hero(        //Hero로 왔다갔다할 때 이미지가 스타일리쉬적으로 넘어갈수잇게
+            tag: widget.data["cid"],  //데이터에서 추가한 cid로 판별
+            child: CarouselSlider(      //캐러설 슬라이드 패키지적용
+              items: imgList.map((map){       //이미지 리스트를 맵으로 돌려서 하나씩 빼옴   //itmes = 슬라이드를 보여줄 이미지
+                return Image.asset(map["url"],
+                  width: size.width,
+                  fit: BoxFit.fill,
+                );
+              }).toList(),                   //리스트는 항상 toList로 빼줘야함
+              options: CarouselOptions(
+                height: size.width,        //height는 width의 크기만큼 오라고 width사이즈를줌
+                initialPage: 0,            //기본페이지는 0번
+                enableInfiniteScroll: true, //사진 스크롤이 무한으로 넘어가는것을 막음
+                viewportFraction: 1,          //1로 두게되면 화면사이즈를 1로차지함.
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+            ),
+            // child:
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,         // 렣프트 라이트 0을 사용하면 오른쪽 왼쪽 다사용하게되어서 가운데로감!!!
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.map((map){
+                return Container(
+                  height: 10.0,
+                  width : 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 4.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,                //BoxDecoration 인디케이터 만들때 박스쉐이프 써클로만들자!!
+                    color: _current == int.parse(map["id"])
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.4),
+
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
       ),
-    ),
+    );
+  }
+  Widget _sellerSimpleInfo(){
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/user.png"),
+            radius: 25,
+          ),
+          SizedBox(width: 10,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('개발하는남자',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+              Text('제주도 도담시',style: TextStyle(fontSize: 13),),
+            ],
+          ),
+          Expanded(child: ManorTemperature(manorTemp: 37.5,)),
+
+        ],
+      ),
+    );
+  }
+
+Widget _bodyWidget(){
+  return Column(
+    children: [
+      _makeSilderImage(),
+      _sellerSimpleInfo(),
+    ],
   );
   }
 }
